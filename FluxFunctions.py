a54f55e174dc13a5dac11c5c9830ea8914b639bb
Ian Holloway <imsnazzy@gmail.com>
2021-01-31 17:53:01 -0500

    Updated Flux functions and derivatives - need to test

--- FluxFunctions.py
+++ FluxFunctions.py
@@ -16,104 +16,62 @@ def pressure(rho,en):
 
 
 def gradP(rho,en):
-    numVerts = len(rho)
+    numNodes = len(rho)
     
-    dP = np.array([(gamma-1)*en, np.zeros([numVerts,2]), (gamma-1)*rho])
+    dP = np.column_stack([(gamma-1)*en, np.zeros([numNodes,2]), (gamma-1)*rho])
     
     return dP
 
 
-def df123dU(rho,v1,v2,en):
-
-    from scipy.sparse import coo_matrix
+def df12dU(rho,v1,v2,en):
+    numNodes = len(rho)
+    
+    from scipy.sparse import csr_matrix
     
     df1rho,df1v1,df1v2,df1E,\
         df2rho,df2v1,df2v2,df2E = dfAll(rho,v1,v2,en)
     
 
     #number of entries in Jacobian = num vars (rho - e) squared 
-    #times numCells = 5*5*numCells
-    numEntries = 5*5*numCells
+    #times numNodes = 4*4*numNodes
+    numEntries = 4*4*numNodes
     
-    rows = np.zeros([numEntries,1])
-    cols = np.zeros([numEntries,1])
-    vals1 = np.zeros([numEntries,1])
-    vals2 = np.zeros([numEntries,1])
-    vals3 = np.zeros([numEntries,1])
+    rows = np.arange(0,numEntries+1,4)
+    cols = np.tile( np.arange(4), 4*numNodes) + np.tile( np.arange(numNodes), 4)
+    vals1 = np.column_stack([df1rho,df1v1,df1v2,df1E]).reshape([-1])
+    vals2 = np.column_stack([df2rho,df2v1,df2v2,df2E]).reshape([-1])
     
-    oneToNumCells = np.arange(numCells).transpose()
     
-    #going left to right top to bottom
-    for i in range(4):
-        rows[oneToNumCells + i*numCells] = oneToNumCells
-        cols[oneToNumCells + i*numCells] = oneToNumCells + i*numCells
-        vals1[oneToNumCells + i*numCells] = df1rho[:,i]
-        vals2[oneToNumCells + i*numCells] = df2rho[:,i]
-        vals3[oneToNumCells + i*numCells] = df3rho[:,i]
-
-    
-    for i in range(4):
-        rows[oneToNumCells + [i+5]*numCells] = oneToNumCells + numCells
-        cols[oneToNumCells + [i+5]*numCells] = oneToNumCells + i*numCells
-        vals1[oneToNumCells + [i+5]*numCells] = df1v1[:,i]
-        vals2[oneToNumCells + [i+5]*numCells] = df2v1[:,i]
-        vals3[oneToNumCells + [i+5]*numCells] = df3v1[:,i]
-
-    
-    for i in range(4):
-        rows[oneToNumCells + [i+10]*numCells] = oneToNumCells + 2*numCells
-        cols[oneToNumCells + [i+10]*numCells] = oneToNumCells + i*numCells
-        vals1[oneToNumCells + [i+10]*numCells] = df1v2[:,i]
-        vals2[oneToNumCells + [i+10]*numCells] = df2v2[:,i]
-        vals3[oneToNumCells + [i+10]*numCells] = df3v2[:,i]
-
-    
-    for i in range(4):
-        rows[oneToNumCells + [i+15]*numCells] = oneToNumCells + 3*numCells
-        cols[oneToNumCells + [i+15]*numCells] = oneToNumCells + i*numCells
-        vals1[oneToNumCells + [i+15]*numCells] = df1V3[:,i]
-        vals2[oneToNumCells + [i+15]*numCells] = df2V3[:,i]
-        vals3[oneToNumCells + [i+15]*numCells] = df3V3[:,i]
-
-    
-    for i in range(4):
-        rows[oneToNumCells + [i+20]*numCells] = oneToNumCells + 4*numCells
-        cols[oneToNumCells + [i+20]*numCells] = oneToNumCells + i*numCells
-        vals1[oneToNumCells + [i+20]*numCells] = df1E[:,i]
-        vals2[oneToNumCells + [i+20]*numCells] = df2E[:,i]
-        vals3[oneToNumCells + [i+20]*numCells] = df3E[:,i]
-
-    
-    df1dU = coo_matrix(vals1, (rows,cols), shape=(5*numCells, 5*numCells) )
-    df2dU = coo_matrix(vals2, (rows,cols), shape=(5*numCells, 5*numCells) )
+    df1dU = csr_matrix( (vals1,rows,cols), shape=(4*numNodes, 4*numNodes) )
+    df2dU = csr_matrix( (vals2,rows,cols), shape=(4*numNodes, 4*numNodes) )
     
     return df1dU, df2dU
 
 
 
-#This is not done
-def dfAll(rho,v1,v2,V3,en):
-    global numCells
+#This might be done now
+def dfAll(rho,v1,v2,en):
+    numNodes = len(rho)
 
     P = pressure(rho,en)
     dP = gradP(rho,en)
     
-    allZero = np.zeros(numCells)
+    allZero = np.zeros(numNodes)
     
     E = en + .5*( v1*v1 + v2*v2 )
-    dE = np.array([allZero, allZero, allZero, V3, np.ones(numCells)]) + .5*GR.gradCrossFlowMag2(v1,v2,gg) 
+    dE = np.column_stack([allZero, v1, v2, np.ones(numNodes)])
     
     
-    df1_rho = np.array([v1, rho, allZero, allZero, allZero])
-    df1_v1 = np.array([v1*v1, 2*rho*v1, allZero, allZero, allZero ]) + gInv[1,1,:]*dP
-    df1_v2 = np.array([v2*v1, rho*v2, rho*v1, allZero, allZero]) + gInv[2,1,:]*dP
-    df1_E = np.array([allZero, (rho*E+P), allZero, allZero, allZero]) + v1*( [E, allZero, allZero, allZero, allZero] + rho*dE + dP )
+    df1_rho = np.column_stack([v1, rho, allZero, allZero])
+    df1_v1 = np.column_stack([v1*v1, 2*rho*v1, allZero, allZero ]) + dP
+    df1_v2 = np.column_stack([v2*v1, rho*v2, rho*v1, allZero]) + dP
+    df1_E = np.column_stack([allZero, (rho*E+P), allZero, allZero]) + v1*( np.column_stack([E, allZero, allZero, allZero]) + rho*dE + dP )
     
     
-    df2_rho = np.array([v2, allZero, rho, allZero, allZero])
-    df2_v1 = np.array([v1*v2, rho*v2, rho*v1, allZero, allZero ]) + gInv[1,2,:]*dP
-    df2_v2 = np.array([v2*v2, allZero, 2*rho*v2, allZero, allZero ]) + gInv[2,2,:]*dP
-    df2_E = np.array([allZero, allZero, (rho*E+P), allZero, allZero]) + v2*( [E, allZero, allZero, allZero, allZero] + rho*dE + dP ) 
+    df2_rho = np.column_stack([v2, allZero, rho, allZero])
+    df2_v1 = np.column_stack([v1*v2, rho*v2, rho*v1, allZero ]) + dP
+    df2_v2 = np.column_stack([v2*v2, allZero, 2*rho*v2, allZero ]) + dP
+    df2_E = np.column_stack([allZero, allZero, (rho*E+P), allZero]) + v2*( np.column_stack([E, allZero, allZero, allZero]) + rho*dE + dP ) 
     
     
 
@@ -122,7 +80,7 @@ def dfAll(rho,v1,v2,V3,en):
 
 
 
-def F123(rho,v1,v2,en):
+def F12(rho,v1,v2,en):
 
     f1_rho,f1_v1,f1_v2,f1_E,\
         f2_rho,f2_v1,f2_v2,f2_E = fAll(rho,v1,v2,en)
