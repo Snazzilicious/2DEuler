07af7db67c7dd8d565c8a8280a46e77974a25666
Ian Holloway <imsnazzy@gmail.com>
2021-02-15 10:42:31 -0500

    Should be fully functional now

--- Main.py
+++ Main.py
@@ -57,6 +57,14 @@ outerIndices = np.array(outerIndices)
 bodyInd = MS.groupNames.index('Body')
 bodyIndices = np.array(MS.groupMembers[bodyInd]) # Don't sort these
 
+nBodyNodes = len(bodyIndices)
+
+# Selecting which direction to enforce no penetration in
+whichDir = np.argmax(np.abs(MS.bodyNodeNormals),axis=1)
+
+bodyBCInds = np.zeros(nBodyNodes).astype(int)
+bodyBCInds[ whichDir==0 ] = v1Indices[ bodyIndices[whichDir==0] ]
+bodyBCInds[ whichDir==1 ] = v2Indices[ bodyIndices[whichDir==1] ]
 
 
 
@@ -68,11 +76,16 @@ MM = GradGradBuilder.getFullSparse().tolil()
 M1 = Mixed1Builder.getFullSparse().tolil()
 M2 = Mixed2Builder.getFullSparse().tolil()
 
+# Zero out equaitons for Dirichlet conditions at infinity
 for i in range(NUM_VARS):
 	MM[outerIndices+i*MS.nNodes,:] = 0
 	M1[outerIndices+i*MS.nNodes,:] = 0
 	M2[outerIndices+i*MS.nNodes,:] = 0
 
+# Zero out equaitons for no penetration conditions at object
+MM[bodyBCInds,:] = 0
+M1[bodyBCInds,:] = 0
+M2[bodyBCInds,:] = 0
 
 MM = MM.tocsr()
 M1 = M1.tocsr()
@@ -89,40 +102,68 @@ U[enIndices] = en_inf
 
 # Printing inital Solution
 MS.printResults(U[rhoIndices], U[v1Indices], U[v2Indices], U[enIndices], 0)
+printTag = 1
+
+
+def adjustBodyVelocity(U,it):
+	for i in range(nBodyNodes):
+		v = np.array([ U[v1Indices[bodyIndices[i]]], U[v2Indices[bodyIndices[i]]] ])
+		amt = v.dot(MS.bodyNodeNormals[i,:])
+		
+		v -= (1/(numIts-it+1))*amt*MS.bodyNodeNormals[i,:]
+		
+		U[v1Indices[bodyIndices[i]]] = v[0]
+		U[v2Indices[bodyIndices[i]]] = v[1]
+		
 
 
 
+print("Beginning Main Loop", flush=True)
+#for it in range(1,numIts+1):
 for it in range(1,numIts+1):
-
-	# Compute flux functions at each node
-	F1, F2 = FF.F12(U[rhoIndices], U[v1Indices], U[v2Indices], U[enIndices])
-	# Compute Jacobian of each flux function
-	DF1DU, DF2DU = FF.df12dU(U[rhoIndices], U[v1Indices], U[v2Indices], U[enIndices])
-
-	# Compute Residual
-	F = (M1 @ F1) + (M2 @ F2) - visc*( MM @ U )
-	if np.linalg.norm(F) > 1e9 :
-		print("Blew Up!")
-		break
 	
-	# Compute Jacobian of Residual
-	DFDU = (M1 @ DF1DU) + (M2 @ DF2DU) - visc*MM
-
-	# Enforce Boundary Conditions
-	DFDU = DFDU.tolil()
-	for i in range(NUM_VARS):
-		for j in range(len(outerIndices)):
-			DFDU[ outerIndices[j]+i*MS.nNodes, outerIndices[j]+i*MS.nNodes ] = 1
-
-
+	adjustBodyVelocity(U,it)	
 	
-	DFDU = DFDU.tocsr()
+	for subIt in range(numSubIts):
+		# Compute flux functions at each node
+		F1, F2 = FF.F12(U[rhoIndices], U[v1Indices], U[v2Indices], U[enIndices])
+		# Compute Jacobian of each flux function
+		DF1DU, DF2DU = FF.df12dU(U[rhoIndices], U[v1Indices], U[v2Indices], U[enIndices])
 
-	del_U = spsolve(DFDU, -F)
-	U += del_U
+		# Compute Residual
+		F = (M1 @ F1) + (M2 @ F2) - visc*( MM @ U )
+		err = np.linalg.norm(F)
+		if err > 1e9 :
+			print("Blew Up!")
+			break
+		if err < 1e-9 :
+			print("Converged")
+			break
+	
+		# Compute Jacobian of Residual
+		DFDU = (M1 @ DF1DU) + (M2 @ DF2DU) - visc*MM
+
+		# Enforce Boundary Conditions
+		DFDU = DFDU.tolil()
+		for i in range(NUM_VARS):
+			for j in range(len(outerIndices)):
+				DFDU[ outerIndices[j]+i*MS.nNodes, outerIndices[j]+i*MS.nNodes ] = 1
+
+		# Enforce No Penetration
+		for i in range(nBodyNodes):
+			DFDU[ bodyBCInds[i], v1Indices[bodyIndices[i]] ] = MS.bodyNodeNormals[i,0]
+			DFDU[ bodyBCInds[i], v2Indices[bodyIndices[i]] ] = MS.bodyNodeNormals[i,1]
+	
+		DFDU = DFDU.tocsr()
+
+		del_U = spsolve(DFDU, -F)
+		U += del_U
 
 	# Printing Solution
-	MS.printResults(U[rhoIndices], U[v1Indices], U[v2Indices], U[enIndices], it)
-
+	MS.printResults(U[rhoIndices], U[v1Indices], U[v2Indices], U[enIndices], printTag)
+	printTag += 1
+	
+	if err > 1e9 or err < 1e-9 :
+		break
 
 print(np.linalg.norm(F))
